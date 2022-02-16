2a7f727c112f45524f9ecb7bb35796b0458e5555
fiekzz <fikrichuck@gmail.com>
2022-02-17 05:15:09 +0800

    gg

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,4 @@
-import React, { Component, useRef } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
