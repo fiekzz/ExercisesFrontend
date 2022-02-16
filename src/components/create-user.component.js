263eb18ad69b3e5185b937ec241d6d89cbf736c3
fiekzz <fikrichuck@gmail.com>
2022-02-17 05:08:55 +0800

    latest commit

---
+++ src/components/create-user.component.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+
+const CreateUsers = () => {
+  const [userName, setUsername] = useState("");
+
+  const newUser = (e) => {
+    e.preventDefault();
+    console.log(userName);
+    axios
+      .post("http://34.124.158.95/users/add", {
+        username: userName,
+      })
+      .then((res) => {
+        console.log(res);
+        alert(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  return (
+    <div>
+      <Form onSubmit={newUser}>
+        <Form.Group className="mb-3">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            onChange={onChangeUsername}
+            style={{ textTransform: "uppercase" }}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <style jsx global>{`
+        ::placeholder {
+          text-transform: initial !important;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default CreateUsers;
---
+++ src/components/exercise-list.component.js
@@ -0,0 +1,49 @@
+import Table from "react-bootstrap/Table";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Exercises = () => {
+  const [Users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://34.124.158.95/exercises")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Duration</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Users.map((item, index) => {
+            return (
+              <tr key={`item-${index}`}>
+                <td>{index + 1}</td>
+                <td>{item.username}</td>
+                <td>{item.description}</td>
+                <td>{item.duration}</td>
+                <td>{new Date(item.date).toLocaleDateString()}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default Exercises;
